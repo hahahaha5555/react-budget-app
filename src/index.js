b7a8bd06aeec03f3cbc1cb6d4eba7e910345cfda
soeon <soeonkim77@gmail.com>
2024-10-16 23:43:38 +0700

    지출앱 만들기

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { AppContextProvider } from './contexts/AppContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <AppContextProvider>
     <App />
-  </React.StrictMode>
+  </AppContextProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
